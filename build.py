372a787d738bfe41134fb4bde3b919a31a9bf6c9
theirix <theirix@gmail.com>
2019-06-15 19:04:17 +0300

    Add windows builds without app option
    It is blocked by missing libev on Windows

--- build.py
+++ build.py
@@ -2,10 +2,18 @@
 # -*- coding: utf-8 -*-
 
 
+import copy
 from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
+
+    for item in copy.copy(builder.items):
+        if item.settings["compiler"] == "Visual Studio" or True:
+            new_options = copy.copy(item.options)
+            new_options["nghttp2:with_app"] = False
+            builder.add(settings=item.settings, options=new_options,
+                        env_vars=item.env_vars, build_requires=item.build_requires)
 
     builder.run()

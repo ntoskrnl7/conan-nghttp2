d8f57e65ed5f3f7026d8a7260d3cce5ea6118ede
theirix <theirix@gmail.com>
2019-06-15 18:49:48 +0300

    Update example

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -4,13 +4,13 @@
 
 int main()
 {
-	nghttp2_info* info = nghttp2_version(NGHTTP2_VERSION_NUM);
+    nghttp2_info* info = nghttp2_version(NGHTTP2_VERSION_NUM);
     if (info) {
         printf("nghttp2 ver=%d version=%s\n", info->version_num, info->version_str);
     } else {
         printf("nghttp2: cannot get version\n");
     }
-	return 0;
+    return 0;
 }
 
 // vim: et ts=4 sw=4

0ff535933eb2130a2be1da628348c1499b2bd0d3
theirix <theirix@gmail.com>
2019-06-16 14:00:58 +0300

    Add asio builds for specific configurations
    Which do not miss dependencies

--- build.py
+++ build.py
@@ -16,5 +16,16 @@ if __name__ == "__main__":
             new_options["nghttp2:with_hpack"] = False
             builder.add(settings=item.settings, options=new_options,
                         env_vars=item.env_vars, build_requires=item.build_requires)
+        # add asio builds for specific configurations which do not miss dependencies
+        if (item.settings["compiler"] == "clang" and \
+            item.settings["compiler.version"] == "7.0" and \
+            item.settings["build_type"] == "Release") or \
+           (item.settings["compiler"] == "Visual Studio" and \
+             item.settings["compiler.version"] == "15" and \
+             item.settings["build_type"] == "Release"):
+            new_options = copy.copy(item.options)
+            new_options["nghttp2:with_asio"] = True
+            builder.add(settings=item.settings, options=new_options,
+                        env_vars=item.env_vars, build_requires=item.build_requires)
 
     builder.run()

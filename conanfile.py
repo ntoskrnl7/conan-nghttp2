17957c850dcc2c8341c26b626f7386384ca8b4ee
JK.LEE <ntoskrnl7@gmail.com>
2019-08-04 16:58:53 +0900

    remove msvc code

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,29 @@
 # -*- coding: utf-8 -*-
 
 from conans import ConanFile, CMake, AutoToolsBuildEnvironment, tools
+from conans.errors import ConanInvalidConfiguration
 import os
 import shutil
 
+util_cpp_asn1_ptime_locale_code = """
+static const std::locale
+    asn1_ptime_locale(std::locale::classic(),
+                 new bt::time_input_facet("%b %d %H:%M:%S %Y GMT"));
+"""
+
+util_cpp_parse_openssl_asn1_time_print_code = """
+#ifdef _WIN32
+  // there is no strptime - use boost
+  std::stringstream sstr(s.str());
+  sstr.imbue(asn1_ptime_locale);
+  bt::ptime ltime;
+  sstr >> ltime;
+  if (!sstr)
+    return 0;
+
+  return boost::posix_time::to_time_t(ltime);
+#else  // !_WIN32
+"""
 
 class Nghttp2Conan(ConanFile):
     name = "nghttp2"
@@ -41,17 +61,16 @@ class Nghttp2Conan(ConanFile):
     def requirements(self):
         self.requires.add("zlib/1.2.11@conan/stable")
         if self.options.with_app:
-            self.requires.add("OpenSSL/1.0.2r@conan/stable")
+            self.requires.add("OpenSSL/1.1.1d@conan/stable")
             self.requires.add("c-ares/1.15.0@conan/stable")
             self.requires.add("libev/4.25@bincrafters/stable")
             self.requires.add("libxml2/2.9.9@bincrafters/stable")
         if self.options.with_hpack:
             self.requires.add("jansson/2.12@bincrafters/stable")
         if self.options.with_asio:
-            self.requires.add("boost/1.68.0@conan/stable")
-            # self.requires.add("boost_asio/1.69.0@bincrafters/stable")
-            # self.requires.add("boost_system/1.69.0@bincrafters/stable")
-            # self.requires.add("boost_thread/1.69.0@bincrafters/stable")
+            self.requires.add("boost_asio/1.70.0@bincrafters/stable")
+            self.requires.add("boost_system/1.70.0@bincrafters/stable")
+            self.requires.add("boost_thread/1.70.0@bincrafters/stable")
 
     def source(self):
         checksum = "8f306995b2805f9f62e9bc042bbf48eb64f6d30b25c04f76cb75d2977d1dd994"
@@ -79,10 +98,11 @@ class Nghttp2Conan(ConanFile):
 
         if self.options.with_app:
             cmake.definitions['OPENSSL_ROOT_DIR'] = self.deps_cpp_info['OpenSSL'].rootpath
-        if self.options.with_asio:
-            cmake.definitions['BOOST_ROOT'] = self.deps_cpp_info['boost'].rootpath
         cmake.definitions['ZLIB_ROOT'] = self.deps_cpp_info['zlib'].rootpath
 
+        cmake.definitions["CMAKE_INSTALL_PREFIX"] = self.package_folder
+        cmake.definitions["CMAKE_INSTALL_LIBDIR"] = "lib"
+        cmake.definitions["CMAKE_INSTALL_BINDIR"] = "bin"
         cmake.configure()
         return cmake
 
@@ -129,7 +149,32 @@ class Nghttp2Conan(ConanFile):
             env_build.make(args=['install'])
 
     def build(self):
-        if self.settings.compiler == "Visual Studio":
+        if self.settings.os == "Windows":
+            if self.options.with_asio:
+                tools.replace_in_file(
+                    'source_subfolder/CMakeLists.txt',
+                    'find_package(Boost ',
+                    '# find_package(Boost ')
+                tools.replace_in_file(
+                    'source_subfolder/src/CMakeLists.txt',
+                    'target_link_libraries(nghttp2_asio',
+                    'target_link_libraries(nghttp2_asio\n\t\tcrypt32\n\t\tws2_32\n\t\tmswsock')
+            tools.replace_in_file(
+                'source_subfolder/src/util.cc',
+                '#include "timegm.h"',
+                '#include "timegm.h"\ninline struct tm *gmtime_r(time_t const *timep, struct tm *tmp) { if (gmtime_s(tmp, timep) == 0) return tmp; return 0;}\ninline struct tm *localtime_r(time_t const *timep, struct tm *tmp) { if (localtime_s(tmp, timep) == 0) return tmp; return 0; }')
+            tools.replace_in_file(
+                'source_subfolder/src/util.cc',
+                'new bt::time_input_facet("%a, %d %b %Y %H:%M:%S GMT"));',
+                'new bt::time_input_facet("%a, %d %b %Y %H:%M:%S GMT"));' + util_cpp_asn1_ptime_locale_code)
+            tools.replace_in_file(
+                'source_subfolder/src/util.cc',
+                'time_t parse_openssl_asn1_time_print(const StringRef &s) {',
+                'time_t parse_openssl_asn1_time_print(const StringRef &s) {' + util_cpp_parse_openssl_asn1_time_print_code)
+            tools.replace_in_file(
+                'source_subfolder/src/util.cc',
+                'return nghttp2_timegm_without_yday(&tm);\n}',
+                'return nghttp2_timegm_without_yday(&tm);\n#endif // !_WIN32\n}')
             cmake = self._configure_cmake()
             cmake.build()
         else:
@@ -137,7 +182,7 @@ class Nghttp2Conan(ConanFile):
 
     def package(self):
         self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
-        if self.settings.compiler == "Visual Studio":
+        if self.settings.os == "Windows":
             cmake = self._configure_cmake()
             cmake.install()
             cmake.patch_config_paths()
